b496d9d0402bd81608d01d8c16a29dad8bf42d35
Leif Christian <leifachristian@gmail.com>
2024-12-04 10:43:33 -0400

    portfolio update #2

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,8 +5,8 @@ export const About: React.FC = () => (
     <h2 className="text-3xl font-bold mb-4">About Me</h2>
     <p className="text-lg">Leif Christian grew up in Paradise Valley, Montana, and is a lover of music, culture and the outdoors.  Leif began his career as a software developer in 2018, specializing in React, Node.js, Vue, React Native, Electron and all things JavaScript.{"\n"}
 
-Leif has collaborated with renowned companies such as Verizon, Magic Mountain Roadrunner Sports, Cedar Fair and United locating Services, a utility locating company serving all of Austin, Dallas, Houston and Tulsa, Oklahoma.
+Leif has collaborated with renowned companies such as Verizon, Magic Mountain, Roadrunner Sports, Cedar Fair and many others to build robust, performant client facing apps to exacting specifications, working teams to refine and deliver high quality software solutions. Leif led a redesign of the client dashboard for United locating Services, a utility locating company handling all 811 tickets for the cities of Austin, Dallas, Houston and Tulsa, Oklahoma. Leif is active in the LLM space, and pioneered a voice based LLM chatbot many months before the release of popular voice mode LLM systems. 
 
-Leif is passionate about music, the outdoors, Spanish language and culture, and music performance. Leif  performs around Montana as guitarist and vocalist in his rock power trio. Leif brings wide range of technical skills and his passion for building engaging user experiences to the team.</p>
+Leif is a professionally trained classical pianist and electric guitarist, and in his free time performs around Montana solo, with various ensembles, and with his power trio. </p>
   </div>
 );

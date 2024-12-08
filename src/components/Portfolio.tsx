a0dffcd465b675a8b99e4b570b6e0ab661419ce4
Leif Christian <leifachristian@gmail.com>
2024-12-07 23:15:17 -0400

    portfolio to top

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,9 +1,14 @@
 // src/components/Portfolio.tsx
-import React from 'react';
+import React, {useEffect} from 'react';
 import PortfolioCard from './PortfolioCard';
 import { portfolioData } from './portfolioData';
 
 const Portfolio = () => {
+
+  useEffect(()=> {
+    window.scrollTo({ top: 0, behavior: 'instant' });
+  }, [])
+
   return (
     <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
       {portfolioData.map((project, idx) => (

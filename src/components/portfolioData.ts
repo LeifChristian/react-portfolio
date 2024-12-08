3c6b4b1991efebecfc6908bfcf3c2679bea350cb
Leif Christian <leifachristian@gmail.com>
2024-12-07 22:37:37 -0400

    update Myplaces description

--- src/components/portfolioData.ts
+++ src/components/portfolioData.ts
@@ -1,45 +1,3 @@
-// export const portfolioData = [
-//   {
-//     title: 'Hustle Fundraising (React, MongoDB)',
-//     images: [
-//       // 'https://picsum.photos/600/400?random=1',
-//       // 'https://picsum.photos/600/400?random=2',
-//       // 'https://picsum.photos/600/400?random=3',
-//     ],
-//     videoUrls: [
-//       'https://www.youtube.com/embed/V-ySIouF6fQ'  // Note the 'embed' in the URL
-//     ],
-//     link: 'https://hustlefundraise.com',
-//     description: 'MERN stack fundraising app for fraternities and sororities'
-//   },
-//   {
-//     title: 'MyPlaces (React Native)',
-//     images: [
-//       // 'https://picsum.photos/600/400?random=9',
-//       // 'https://picsum.photos/600/400?random=10',
-//       // 'https://picsum.photos/600/400?random=11',
-//       // 'https://picsum.photos/600/400?random=12',
-//     ],
-//     videoUrls: [
-//       'https://www.youtube.com/embed/9Q0JQhoIebA'  // Note the 'embed' in the URL
-//     ],
-//     link: 'https://github.com/LeifChristian/myPlaces-EAS-Expo',
-//     description: 'React Native Maps applicaton for saving places, google search for places, directions and simple one handed navigation. Super useful for travel!'
-//   },
-//   {
-//     title: 'File Explorer (Vue.js, Electron)',
-//     images: [
-//       // 'https://picsum.photos/600/400?random=6',
-//       // 'https://picsum.photos/600/400?random=7',
-//       // 'https://picsum.photos/600/400?random=8',
-//     ],
-//     videoUrls: [
-//       'https://www.youtube.com/embed/2cH0N8azup4'  // Note the 'embed' in the URL
-//     ],
-//     link: 'https://github.com/LeifChristian/electron-vue-GUI-file-explorer',
-//     description: 'Explore the file system, see all available drives and search for files. Files open with their respective programs. Works for Mac OS, windows and Linux (with X-server installed)'
-//   },
-// ];
 
 export const portfolioData = [
   {
@@ -61,13 +19,13 @@ export const portfolioData = [
     description: 'MERN stack fundraising app for fraternities and sororities'
   },
   {
-    title: 'MyPlaces (React Native)',
+    title: 'MyPlaces Location App (React Native)',
     images: [],
     videoUrls: [
       'https://www.youtube.com/embed/9Q0JQhoIebA'
     ],
     link: 'https://github.com/LeifChristian/myPlaces-EAS-Expo',
-    description: 'React Native Maps applicaton for saving places, google search for places, directions and simple one handed navigation. Super useful for travel!'
+    description: 'React Native Maps applicaton for saving places. Google search for places and easy save pins, directions, one handed navigation. Very useful for travel!'
   },
   {
     title: 'File Explorer (Vue.js, Electron)',
